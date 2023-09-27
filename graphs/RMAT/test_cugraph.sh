d02e0cde85fed3834e645fe1fa3181d6b496eba4
greg <go2432@wayne.edu>
2023-09-26 23:20:06 -0500

    A

--- graphs/RMAT/test_cugraph.sh
+++ graphs/RMAT/test_cugraph.sh
@@ -4,16 +4,15 @@
 C=10
 for i in {5..5}
 do
-for j in {1..20}
+for j in {5..20}
 do
-let SCALE=10**$i   # sets SCALE to 10Ei.
-VERTICES=$(($j*$SCALE))
+let VERTICES=2**$i   # sets SCALE to 10Ei.
 EDGES=$(($VERTICES*$C))
 filename="${VERTICES}_${EDGES}_RMAT.txt"
 a=0.45
 b=0.22
 c=0.22
-python generate_rmat_graph.py $VERTICES $EDGES $a $b $c $filename 1
+python generate_rmat_graph.py $j $EDGES $a $b $c $filename 1
 ../../src/matching $filenameKece
 rm $filenameKece
 #cp $filenameShifted $filenameBlossV
