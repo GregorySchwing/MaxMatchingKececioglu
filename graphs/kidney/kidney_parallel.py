5d7f57b5a880441d2ecf163e7327eea9ee34b154
greg <go2432@wayne.edu>
2023-09-28 05:27:07 -0500

    A

--- graphs/kidney/kidney_parallel.py
+++ graphs/kidney/kidney_parallel.py
@@ -19,6 +19,30 @@ def are_compatible(blood_type1, blood_type2):
         return True
     return False
 
+def are_pair_compatible(pair1, pair2):
+    person11, person12 = pair1  # First person in the first pair and second person in the first pair
+    person21, person22 = pair2  # First person in the second pair and second person in the second pair
+
+    # Check compatibility based on your criteria
+    compatible_case1 = are_compatible(person11, person21) and are_compatible(person12, person22)
+    compatible_case2 = are_compatible(person11, person22) and are_compatible(person12, person21)
+
+    return compatible_case1 or compatible_case2
+
+# Define the pair_compatible function
+def pair_compatible_index(successful_pairs, pair1_index, pair2_index):
+    pair1 = successful_pairs[pair1_index]
+    pair2 = successful_pairs[pair2_index]
+
+    person1_a, person1_b = pair1
+    person2_a, person2_b = pair2
+    
+    # Check compatibility in both ways
+    compatible1 = are_compatible(person1_a, person2_a) and are_compatible(person1_b, person2_b)
+    compatible2 = are_compatible(person1_a, person2_b) and are_compatible(person1_b, person2_a)
+    
+    return compatible1 or compatible2
+
 # Determine the number of processes based on the number of threads available
 threads_count = psutil.cpu_count(logical=False)
 num_processes = max(1, int(threads_count))
@@ -62,30 +86,53 @@ edges_per_process = args.M // num_processes
 # Create edge chunks for parallel processing
 edge_chunks = []
 for _ in range(num_processes):
-    edge_chunk = random.sample(G.nodes(), edges_per_process * 2)  # Sample twice as many nodes
-    edge_chunk = [(edge_chunk[i], edge_chunk[i+1]) for i in range(0, len(edge_chunk), 2)]  # Create pairs
+    edge_chunk = random.choices(list(G.nodes()), k=edges_per_process * 2)  # Sample with replacement
+    edge_chunk = [(edge_chunk[i], edge_chunk[i + 1]) for i in range(0, len(edge_chunk), 2)]  # Create pairs
     edge_chunks.append(edge_chunk)
 
 # Function to add edges in parallel
-def add_edges_parallel(args):
-    chunk, node_blood_types, p = args
-    result = []
-    for u, v in chunk:
-        blood_type_u = node_blood_types[u]
-        blood_type_v = node_blood_types[v]
-        if are_compatible(blood_type_u, blood_type_v) or random.random() <= p:
-            result.append((u, v))
-    return result
+def evaluate_pairs(args):
+    N, blood_types, compatibility_threshold, probability_pc = args
+    num_samples = 2 * N // num_processes  # Sample N/number of processes pairs
+    pairs_to_evaluate = random.choices(blood_types, k=num_samples)  # Sample blood types
 
-# Add edges in parallel without progress bars
-with Pool(num_processes) as pool:
-    result_edges = pool.map(add_edges_parallel, [(chunk, nx.get_node_attributes(G, 'blood_type'), 0.2) for chunk in edge_chunks])
+    successful_pairs = []
+    for i in range(0, len(pairs_to_evaluate), 2):
+        blood_type_u, blood_type_v = pairs_to_evaluate[i], pairs_to_evaluate[i + 1]
+        if not are_compatible(blood_type_u, blood_type_v) or random.uniform(0, 1) <= compatibility_threshold:
+            successful_pairs.append((blood_type_u, blood_type_v))
+    return successful_pairs
 
-# Flatten the list of edge lists
-edges_to_add = [edge for sublist in result_edges for edge in sublist]
+result_vertices = []  # Changed variable name
 
+# Keep running until the length of result_vertices > N
+while len(result_vertices) <= args.N:  # Changed variable name
+    # Add edges in parallel without progress bars
+    with Pool(num_processes) as pool:
+        result_edges_batch = pool.map(evaluate_pairs, [(args.N, nx.get_node_attributes(G, 'blood_type'), 0.2, 0.2) for _ in range(num_processes)])
+
+    # Flatten the list of edge lists and extend the result_vertices list
+    edges_batch = [edge for sublist in result_edges_batch for edge in sublist]
+    result_vertices.extend(edges_batch)  # Changed variable name
+
+# Remove extra edges if more than N edges were generated
+result_vertices = result_vertices[:args.N]  # Changed variable name
+
+from itertools import combinations_with_replacement, islice, tee
+
+n_processes = num_processes
+n = args.N  # num cols/rows in matrix
+
+pairs = ((i, j) for i, j in combinations_with_replacement(range(n), 2) if i != j)
+pair_chunks = [
+    list(islice(p, i, None, n_processes))
+    for i, p in enumerate(tee(pairs, n_processes))
+]
+
+print(pair_chunks)
+quit()
 # Add the edges to the graph
-G.add_edges_from(edges_to_add)
+G.add_edges_from(result_vertices)  # Changed variable name
 
 # Default output file name includes N, M, and blood type probabilities (capitalized)
 # Generate the output file name only if no output argument is provided
@@ -125,4 +172,3 @@ num_connected_components = nx.number_connected_components(G)
 print(f"Number of connected components: {num_connected_components}")
 
 print(f"Random kpd graph with {args.N} vertices and {G.number_of_edges()} edges (vertices incremented by 1) written to {output_file}.")
-
