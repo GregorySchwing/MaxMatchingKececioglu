32a1dd89a5183433a768f2feb4b248fc0e28e70b
greg <go2432@wayne.edu>
2023-09-28 18:16:03 -0500

    A

--- graphs/RMAT/test.sh
+++ graphs/RMAT/test.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 
-C=10
+C=2
+for C in {2..10}
+do
 for i in {5..5}
 do
-for j in {1..20}
+for j in {1..5}
 do
 let SCALE=10**$i   # sets SCALE to 10Ei.
 VERTICES=$(($j*$SCALE))
 EDGES=$(($VERTICES*$C))
-filename="${VERTICES}_${EDGES}_RMAT.txt"
-filenameShifted="${VERTICES}_${EDGES}_RMAT_start_from_1.txt"
-filenameKece="${VERTICES}_${EDGES}_RMAT_Kece.txt"
-filenameBlossV="${VERTICES}_${EDGES}_RMAT_blossV.txt"
-filenameMTX="${VERTICES}_${EDGES}_RMAT.txt.mtx"
-../PaRMAT/Release/PaRMAT -nVertices ${VERTICES} -nEdges ${EDGES} -output $filename -threads 16 -sorted -noEdgeToSelf -noDuplicateEdges -undirected
+filename="${VERTICES}_${EDGES}_${C}_RMAT.txt"
+filenameShifted="${filename}_start_from_1.txt"
+filenameKece="${VERTICES}_${EDGES}_${C}_RMAT_Kece.txt"
+filenameBlossV="${VERTICES}_${EDGES}_${C}_RMAT_blossV.txt"
+filenameMTX="${VERTICES}_${EDGES}_${C}_RMAT.txt.mtx"
+../PaRMAT/Release/PaRMAT -nVertices ${VERTICES} -nEdges ${EDGES} -output $filename -threads 16 -sorted -noEdgeToSelf -noDuplicateEdges -undirected -a 0.45 -b 0.15 -c 0.15 -d 0.25
 
 awk -v s=1 '{print $1+s, $2+s}' $filename > $filenameShifted
 
@@ -37,6 +39,12 @@ sed -i "2s/^/$VERTICES $VERTICES $EDGES\n/" $filenameMTX
 #sed -i "1s/^/p edge $VERTICES $EDGES\n/" $filenameBlossV
 #../../blossom5-v2.04.src/blossom5 -e $filenameBlossV
 
+/home/greg/mvm/src/cpu $filenameMTX
+#/home/greg/mvm/src/a.out $filenameMTX 0 1
+/home/greg/mvm/src/a.out $filenameMTX 60 48
+#rm $filenameMTX
 
+
+done
 done
 done
