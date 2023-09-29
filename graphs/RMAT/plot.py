245fb13567e554a151d1e097f4d7422bec00dd40
greg <go2432@wayne.edu>
2023-09-29 16:51:17 -0500

    closer

--- graphs/RMAT/plot.py
+++ graphs/RMAT/plot.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns  # Import seaborn for colorblind-accessible colors
+from matplotlib.lines import Line2D  # Import Line2D for legend handles
 
 # Read the CSV file for GPUResults (change the path as needed)
 df_gpu = pd.read_csv('GPUResults.csv')
@@ -44,34 +46,50 @@ relative_speedup_df['relative_speedup'] = relative_speedup_df['seconds_CPU'] / r
 # Get unique C values from the combined dataframe
 unique_c_values = relative_speedup_df['C'].unique()
 
-# Check if there are unique C values before creating subplots
-if len(unique_c_values) > 0:
-    # Create a figure with subplots arranged in a grid
-    fig, axs = plt.subplots(len(unique_c_values), 1, figsize=(10, 6 * len(unique_c_values)), sharex=True)
-    fig.suptitle('Relative Speedup vs. V (Grouped by C, NUMSTACK_GPU, and Device)', fontsize=16)
+# Determine the number of subplots needed
+num_subplots = len(unique_c_values)
 
-    # Iterate through unique C values and create subplots in the grid
-    for i, c_value in enumerate(unique_c_values):
-        ax = axs[i]
-        df_c = relative_speedup_df[relative_speedup_df['C'] == c_value]
+# Calculate the number of rows and columns for the subplot grid
+num_rows = int(num_subplots ** 0.5)
+num_cols = (num_subplots + num_rows - 1) // num_rows
 
-        ax.set_title(f'C={c_value}')
-        ax.set_xlabel('V')
-        ax.set_ylabel('Relative Speedup')
+# Define a list of marker symbols
+marker_symbols = ['o', 's', 'D', '^', 'v', '<', '>', 'p', '*', 'H']
 
-        # Plot relative speedup for each NUMSTACK_GPU value
-        for numstack in df_c['NUMSTACK_GPU'].unique():
-            df_numstack = df_c[df_c['NUMSTACK_GPU'] == numstack]
-            label = f'NUMSTACK_GPU={numstack}'
-            ax.plot(df_numstack['V'], df_numstack['relative_speedup'], marker='o', linestyle='-', markersize=6, label=label)
+# Use the colorblind-accessible palette from seaborn
+colors = sns.color_palette("colorblind", num_subplots)
 
-        ax.legend(title='NUMSTACK_GPU')
+# Create a figure with subplots arranged in a grid
+fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+fig.suptitle('Relative Speedup vs. V (Grouped by C, NUMSTACK_GPU, and Device)', fontsize=16)
 
-    # Adjust spacing between subplots
-    plt.tight_layout(rect=[0, 0, 1, 0.97])
+# Flatten the axs array if it's multidimensional
+axs = axs.ravel()
 
-    # Show the plot
-    plt.show()
-else:
-    print("No unique C values found in the combined dataframe.")
+for i, c_value in enumerate(unique_c_values):
+    ax = axs[i]
+    df_c = relative_speedup_df[relative_speedup_df['C'] == c_value]
+
+    ax.set_title(f'C={c_value}')
+    ax.set_xlabel('V')
+    ax.set_ylabel('Relative Speedup')
+
+    # Plot relative speedup for each NUMSTACK_GPU value with different marker symbols and colors
+    legend_handles = []
+    for j, numstack in enumerate(df_c['NUMSTACK_GPU'].unique()):
+        df_numstack = df_c[df_c['NUMSTACK_GPU'] == numstack]
+        label = f'NUMSTACK_GPU={numstack}'
+        marker_symbol = marker_symbols[j % len(marker_symbols)]  # Cycle through marker symbols
+        color = colors[j % num_subplots]  # Cycle through colors
+        line = ax.plot(df_numstack['V'], df_numstack['relative_speedup'], marker=marker_symbol, linestyle='-', markersize=6, label=label, color=color)
+        legend_handles.append(Line2D([0], [0], color=color, marker=marker_symbol, markersize=6, label=label))
+
+# Create a single horizontal legend outside the subplots
+fig.legend(handles=legend_handles, loc='upper center', ncol=len(df_c['NUMSTACK_GPU'].unique()), bbox_to_anchor=(0.5, -0.05))
+
+# Adjust spacing between subplots
+plt.tight_layout(rect=[0, 0, 1, 0.97])
+
+# Show the plot
+plt.show()
 
